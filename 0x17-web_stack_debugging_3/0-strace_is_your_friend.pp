80dbc7c08b937b5d72ec95a3f6db1f86086c86b0
KatliKoma <katlegokoma1@gmail.com>
2024-06-06 02:22:37 +0200

    done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-repair LAMP stack server running wordpress
+# Substitutes `.phpp` with `.php` in the file `wp-settings.php`.
 
 exec { 'replace-phpp':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
